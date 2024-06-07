87a4acfbbd44adec53101bb198c224756ec82c48
Andreas Ulvund <ulvund_a@hotmail.com>
2024-06-07 19:57:00 +0200

    Update ShoppingBagIcon to use styled component for image styling

--- src/components/Icons/ShoppingBagIcon.tsx
+++ src/components/Icons/ShoppingBagIcon.tsx
@@ -1,5 +1,13 @@
+import styled from 'styled-components';
+
+const StyledImage = styled.img`
+  @media screen and (max-width: 768px) {
+    width: 400px;
+  }
+`;
+
 const ShoppingBagIcon = () => {
-  return <img src="./images/shopping-icone.png" alt="heading" />;
+  return <StyledImage src="./images/shopping-icone.png" alt="heading" />;
 };
 
 export default ShoppingBagIcon;
